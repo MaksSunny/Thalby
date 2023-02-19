d97fad1fda7e37e2232ade3d2e0326ca1a7020f5
MaksSunny <vaskovets333@gmail.com>
2023-02-20 01:47:37 +0200

    Thalby + carousel mobile menu

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,17 @@ $(document).ready(function(){
         }
     });
 });
+
+menuBtn = document.querySelector('.menu-btn');
+nav = document.querySelector('nav');
+lineOne = document.querySelector('nav .menu-btn .line--1');
+lineTwo = document.querySelector('nav .menu-btn .line--2');
+lineThree = document.querySelector('nav .menu-btn .line--3');
+link = document.querySelector('nav .nav-links');
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('nav-open');
+    lineOne.classList.toggle('line-cross');
+    lineTwo.classList.toggle('line-fade-out');
+    lineThree.classList.toggle('line-cross');
+    link.classList.toggle('fade-in');
+})
